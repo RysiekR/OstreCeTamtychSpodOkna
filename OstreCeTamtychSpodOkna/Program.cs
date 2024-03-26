eca6df42765296a2d8a288c09832a21afbcf90c2
RysiekR <wrkusmirczak@gmail.com>
2024-03-26 18:42:01 +0100

    Refactor Separation of BattleWindow.cs fromProgram.cs

--- OstreCeTamtychSpodOkna/Program.cs
+++ OstreCeTamtychSpodOkna/Program.cs
@@ -4,7 +4,7 @@ using System.Data;
 using OstreCeTamtychSpodOkna;
 using System.Media;
 using System.Security.Cryptography.X509Certificates;
-public class Program
+public partial class Program
 {
     public static void Main()
     {
@@ -26,91 +26,6 @@ public class Program
         Application.Run(battleWindow);
         
         Application.Shutdown();
-    }
-
-    public class BattleWindow : Window
-    {
-        public BattleWindow(GameState gameState) : base("Battle")
-        {
-            {
-                var attackButton = new Button()
-                {
-                    Text = "Attack!",
-                    X = 10,
-                    Y = 10,
-                    IsDefault = true,
-                };
-                attackButton.Clicked += () =>
-           {
-               gameState.PlayerPokemon.Attack(gameState.EnemyPokemon);
-
-               if (OperatingSystem.IsWindows())
-               {
-                   Console.Beep(1000, 1800);
-               }
-           };
-                var skillsButton = new Button()
-                {
-                    Text = "Skills",
-                    X = 20,
-                    Y = 20,
-                    IsDefault = true,
-                };
-                skillsButton.Clicked += () =>
-                            {
-                                var skillsDialog = new Dialog("Choose a skill", 60, 20)
-                                {
-                                    X = Pos.Center(),
-                                    Y = Pos.Center()
-                                };
-                                int column1XOffset = -30;
-                                int column2XOffset = 2;
-                                int startY = 1;
-                                int spacingY = 2;
-
-                                bool useFirstColumn = true; //Zmienna do przełączania między kolumnami
-
-                                foreach (Skill element in gameState.SkillList)
-                                {
-                                    var skillButton = new Button(element.Name)
-                                    {
-                                        X = Pos.Left(skillsDialog) + (useFirstColumn ? column1XOffset : column2XOffset),
-                                        Y = Pos.Top(skillsDialog) + startY,
-                                        IsDefault = false
-                                    };
-                                    skillButton.Clicked += () =>
-                                    {
-                                        //Logika przycisku skilli
-                                    };
-                                    skillsDialog.Add(skillButton);
-
-                                    if (useFirstColumn)
-                                    {
-                                        useFirstColumn = false;
-                                    }
-                                    else
-                                    {
-                                        useFirstColumn = true;
-                                        startY += spacingY;
-                                    }
-                                }
-                                var differentButton = new Button("Different")
-                                {
-                                    X = 6,
-                                    Y = 6,
-                                    IsDefault = true,
-                                };
-                                skillsDialog.AddButton(differentButton);
-
-                                differentButton.Clicked += () =>
-                                {
-                                    Application.RequestStop();
-                                };
-                                Application.Run(skillsDialog);
-                            };
-                Add(attackButton, skillsButton);
-            }
-        }
     }
 }
 public class Pokemon
