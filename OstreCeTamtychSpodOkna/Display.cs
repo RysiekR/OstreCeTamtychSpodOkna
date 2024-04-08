aa13aa16f0fb0c7666688473c82bb673827ef4c6
RysiekR <wrkusmirczak@gmail.com>
2024-04-08 10:27:42 +0200

    Addaed Display class hopefully to reduce flickering

---
+++ OstreCeTamtychSpodOkna/Display.cs
@@ -0,0 +1,34 @@
+﻿namespace OstreCeTamtychSpodOkna
+{
+    internal static class Display
+    {
+        private static List<string> display = new List<string>(); // ten ktory powinien juz byc wyswietlony, stary
+        private static List<string> newDisplay = new List<string>(); // temp do ktorego trzeba wrzucic zaktualizowana mape do wyswietlenia
+
+
+        // Porównanie buforów, wypisanie ich na ekran i zapis zmian
+        public static void RenderDisplay()
+        {
+            for (int row = 0; row < display.Count; row++)
+            {
+                if (display[row] != newDisplay[row])
+                {
+                    for (int i = 0; i < display[row].Length; i++)
+                    {
+                        if (display[row][i] != newDisplay[row][i])
+                        {
+                            Console.SetCursorPosition(i, row);
+                            Console.Write(newDisplay[row][i]);
+                        }
+                    }
+                }
+            }
+            // Przekopiowanie bufora zmian na bufor renderowania
+            display = newDisplay;
+        }
+        public static void GetNewDisplay(Map map)
+        {
+            newDisplay = map.mapAsList;
+        }
+    }
+}
