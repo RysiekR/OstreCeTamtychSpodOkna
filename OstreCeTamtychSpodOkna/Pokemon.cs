9c4a46ec09ea9d8ad1249fad9212ca9aaf11b421
Mioodek <lukasz.leja.w@gmail.com>
2024-04-23 16:11:21 +0200

    pokemon class fix

--- OstreCeTamtychSpodOkna/Pokemon.cs
+++ OstreCeTamtychSpodOkna/Pokemon.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
   public class Pokemon
 {
 
@@ -49,36 +48,3 @@
     }
 
 }
-=======
-﻿namespace OstreCeTamtychSpodOkna
-{
-    internal class Pokemon
-    {
-        public string Name { get; set; }
-        public int HP { get; set; }
-        public int AttackPower { get; set; }
-        public int HealPower { get; set; }
-        public int MaxHP { get; private set; }//
-
-        public Pokemon(string name, int hp, int attackPower, int healPower)
-        {
-            Name = name;
-            HP = hp;
-            MaxHP = hp;
-            AttackPower = attackPower;
-            HealPower = healPower;
-        }
-
-        public void Attack(Pokemon other)
-        {
-            other.HP -= this.AttackPower;
-            //Console.WriteLine($"{this.Name} zaatakował {other.Name} zadając {this.AttackPower} obrażeń.");
-        }
-        public void Heal()
-        {
-            this.HP += this.HealPower;
-            Console.WriteLine($"{this.Name} użył leczenia, przywracając {this.HealPower} HP.");
-        }
-    }
-}
->>>>>>> origin/main
